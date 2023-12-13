ac9f469abc91f8cf23db916fd3148d07c0c9f799
ivander11 <153476992+ivander11@users.noreply.github.com>
2023-12-13 13:46:08 +0700

    Update TUBES_DASKOM.c

--- TUBES_DASKOM.c
+++ TUBES_DASKOM.c
@@ -133,37 +133,51 @@ void menulogin() {
         break;
     }
 }
-void loginuser(){
+void loginuser() {
     char username[50], password[50];
     char fileUsername[50], filePassword[50], fileNama[50], fileEmail[50], fileNoTelepon[15];
-    int loginSuccess = 0, attempt = 2;
+    int attempt = 3;
 
+    for (attempt; attempt > 0; attempt--) {
+        printf("\nSilahkan Login User\n\n");
+        printf("Username : ");
+        scanf("%s", &username);
 
-    printf("Silahkan Login User\n\n");
-    printf("Username : ");
-    scanf("%s", &username);
+        printf("Password : ");
+        scanf("%s", &password);
 
-    printf("Password : ");
-    scanf("%s", password);
-
-    // Membaca data dari file sekuensial
-    FILE *file = fopen("userdata.dat", "rb");
-    if (file != NULL) {
-        while (fscanf(file, "%s %s %s %s %s", fileUsername, filePassword, fileNama, fileEmail, fileNoTelepon) != EOF) {
-            // Memeriksa apakah username dan password user sesuai
-            if (strcmp(username, fileUsername) == 0 && strcmp(password, filePassword) == 0) {
-                loginSuccess = 1;
-                printf("\nSelamat login berhasil\n");
-                // menuUser();
-                break;
-            }else {
-                printf("Username atau password salah.");
+        FILE *file = fopen("userdata.txt", "rb");
+        if (file != NULL) {
+            while (fscanf(file, "%s %s %s %s %s", fileUsername, filePassword, fileNama, fileEmail, fileNoTelepon) != EOF) {
+                if (strcmp(username, fileUsername) == 0 && strcmp(password, filePassword) == 0) {
+                    printf("\nSelamat login berhasil\n");
+                    fclose(file);
+                    // menuUser();
+                    return;
+                }
             }
+            fclose(file);
+        } else {
+            printf("Error: Tidak dapat membaca file data user.\n");
+            return;
         }
-        fclose(file);
-    }else {
-        printf("Error: Tidak dapat membaca file data user.\n");
+
+        printf("Username atau password salah. Sisa percobaan: %d\n", attempt-1);
+
+        int choice;
+        printf("Pilihan:\n");
+        printf("1. Coba lagi\n");
+        printf("2. Lupa Password\n");
+        printf("Pilih (1-2): ");
+        scanf("%d", &choice);
+
+        if (choice == 2) {
+            // lupaPassword();
+            return;
+        }
     }
+
+    printf("Anda telah melebihi batas percobaan. Login dibatalkan.\n");
 }
 
 
